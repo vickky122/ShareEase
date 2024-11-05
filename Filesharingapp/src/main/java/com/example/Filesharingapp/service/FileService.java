8b889b466bfaf4973a25bc6f1888ef695a5b59d6
Vikrant Yadav <vikrantmani45@gmail.com>
2024-11-05 12:09:41 +0530

    initial file service structure

---
+++ Filesharingapp/src/main/java/com/example/Filesharingapp/service/FileService.java
@@ -0,0 +1,20 @@
+package com.example.Filesharingapp.service;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.Filesharingapp.model.FileModel;
+
+public interface FileService {
+  public List<FileModel> getAll();
+
+  public ResponseEntity<?> uploadFile(MultipartFile file, String uploadedBy) throws IOException;
+
+  public ResponseEntity<?> shareFile(int id);
+
+  public ResponseEntity<?> deleteFile(int id);
+
+}
