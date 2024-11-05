7becced3d218be9422d941ef7be1be7b7092eefd
Vikrant Yadav <vikrantmani45@gmail.com>
2024-11-05 15:13:33 +0530

    minor change

--- Filesharingapp/src/main/java/com/example/Filesharingapp/repository/FileRepository.java
+++ Filesharingapp/src/main/java/com/example/Filesharingapp/repository/FileRepository.java
@@ -1,10 +1,13 @@
 package com.example.Filesharingapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.Filesharingapp.entities.FileEntity;
 import java.util.List;
 import java.time.LocalDateTime;
+
+@Repository
 
 public interface FileRepository extends JpaRepository<FileEntity, String> {
   // auto delete after 24 hours
