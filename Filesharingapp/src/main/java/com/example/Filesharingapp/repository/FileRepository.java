ccf1380b2847deb872477dc03384eeee9224c093
Vikrant Yadav <vikrantmani45@gmail.com>
2024-11-05 11:58:48 +0530

    string in place of integer

--- Filesharingapp/src/main/java/com/example/Filesharingapp/repository/FileRepository.java
+++ Filesharingapp/src/main/java/com/example/Filesharingapp/repository/FileRepository.java
@@ -3,8 +3,11 @@ package com.example.Filesharingapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.Filesharingapp.entities.FileEntity;
+import java.util.List;
+import java.time.LocalDateTime;
 
-public interface FileRepository extends JpaRepository<FileEntity, Integer> {
-  // auto delete after 20 hours
+public interface FileRepository extends JpaRepository<FileEntity, String> {
+  // auto delete after 24 hours
+  List<FileEntity> findByExpiryDateBefore(LocalDateTime now);
 
 }
