50a8926c25db7c684d83363d68399742057f7afb
Vikrant Yadav <vikrantmani45@gmail.com>
2024-11-05 14:55:12 +0530

    structure serviceimpl

--- Filesharingapp/src/main/java/com/example/Filesharingapp/service/FileServiceImpl.java
+++ Filesharingapp/src/main/java/com/example/Filesharingapp/service/FileServiceImpl.java
@@ -1,5 +1,37 @@
 package com.example.Filesharingapp.service;
 
-public class FileServiceImpl {
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.Filesharingapp.model.FileModel;
+
+public class FileServiceImpl implements FileService {
+
+    @Override
+    public List<FileModel> getAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+    }
+
+    @Override
+    public ResponseEntity<?> uploadFile(MultipartFile file, String uploadedBy) throws IOException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'uploadFile'");
+    }
+
+    @Override
+    public ResponseEntity<?> shareFile(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'shareFile'");
+    }
+
+    @Override
+    public ResponseEntity<?> deleteFile(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteFile'");
+    }
 
 }
