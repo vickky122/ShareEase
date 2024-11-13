113d7a30b17c93279e7477bc10ca189c6487a8b8
Vikrant Yadav <vikrantmani45@gmail.com>
2024-11-13 10:15:34 +0530

    entity error resolved

--- Filesharingapp/src/main/java/com/example/Filesharingapp/entities/FileEntity.java
+++ Filesharingapp/src/main/java/com/example/Filesharingapp/entities/FileEntity.java
@@ -10,29 +10,23 @@ import lombok.Data;
 
 @Entity
 @Data
-public class FileEntity {
+public class FileEntity{
 
   @Id
-  // @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private String id;
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
 
   private String fileName;
   private String uploadedBy;
   private LocalDateTime uploadTime;
   private LocalDateTime expiryTime;
 
-  // @ManyToOne
-  // private UserInfo userInfo;
 
   @Lob
-  @Column(name = "file_data", columnDefinition = "LONGBLOB")
+  @Column(name = "file_data", columnDefinition="LONGBLOB")
   private byte[] fileData;
 
-  @PrePersist
-  public void generateId() {
-    if (this.id == null) {
-      this.id = UUID.randomUUID().toString();
-    }
-  }
+
+
 
 }
